add6bfe19f1680f6ce7995a30ca25b006ac54b65
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2016-04-23 21:25:31 -0430

    Adding log message

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.get('/webhook/', function (req, res) {
 });
 
 app.post('/webhook/', function (req, res) {
+    console.log(JSON.stringify(req.body));
   var messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     var event = req.body.entry[0].messaging[i];
