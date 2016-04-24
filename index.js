a2606b1a9a7a41f1728651604eac9cbddf839607
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2016-04-23 21:22:45 -0430

    Adding body parser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var KEY = require('./constants').KEY;
 var _ = require('lodash');
 var sendTextMessage = require('./functions').sendTextMessage;
+
+app.use(bodyParser.urlencoded());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -17,7 +20,6 @@ app.get('/webhook/', function (req, res) {
 });
 
 app.post('/webhook/', function (req, res) {
-    console.log("webhook",JSON.stringify(req.body.entry[0]));
   var messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     var event = req.body.entry[0].messaging[i];
